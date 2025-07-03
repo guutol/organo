51720ba0aa4d33ed330c19865a77eaf29ef997e9
guutol <n4megustavo@gmail.com>
2025-07-03 13:33:48 -0300

    feat: banner da página inicial

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./banner.css"
+
+function Banner(){
+    return (
+        <header className="container_banner">
+            <img src="./imagens/banner.png" alt="Banner da página" />
+        </header>
+    )
+}
+
+export default Banner;
